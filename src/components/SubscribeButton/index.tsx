f297a93e825030d27a246822dc17dcd942995aa0
die-goncalves <die.goncalves1990@gmail.com>
2021-03-29 17:09:09 -0300

    PagamentosNoStripe/Gerando sessao de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,18 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SubscribeButton() {
+    const [ session ] = useSession();
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
             Subscribe now
         </button>
     )
