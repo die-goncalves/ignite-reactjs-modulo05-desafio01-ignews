1af985c73c54b36a6029a34101a154f62027dd27
die-goncalves <die.goncalves1990@gmail.com>
2021-03-31 15:13:19 -0300

    TrabalhandoComCMS/Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import Prismic from '@prismicio/client';
 import styles from './styles.module.scss';
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
 
 export default function Posts(){
     return(
@@ -29,4 +32,20 @@ export default function Posts(){
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publication')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    })
+
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+        props: {}
+    }
 }
