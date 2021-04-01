eb097f3f06bdebf82a1c7cae3676c76261b6638a
die-goncalves <die.goncalves1990@gmail.com>
2021-04-01 11:28:18 -0300

    PaginasEstaticas/Validando assinatura ativa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuth, { Session } from 'next-auth';
 import Providers from 'next-auth/providers';
-
 import { query as q } from 'faunadb';
+
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
@@ -15,6 +15,41 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async session(session: Session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
