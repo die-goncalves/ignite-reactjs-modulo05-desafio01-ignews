b763dd75f596a2ce8d085a1a99c411cc9cf85d49
die-goncalves <die.goncalves1990@gmail.com>
2021-08-19 14:51:25 -0300

    Redirecionar para a versão preview do post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -16,8 +16,8 @@ interface PostsProps {
     posts: Post[]
 }
 
-export default function Posts({ posts }: PostsProps){
-    return(
+export default function Posts({ posts }: PostsProps) {
+    return (
         <>
             <Head>
                 <title>Posts | Ignews</title>
@@ -26,12 +26,12 @@ export default function Posts({ posts }: PostsProps){
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <Link href={`/posts/${post.slug}`}>
-                        <a key={post.slug}>
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
+                        <Link href={`/posts/preview/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
                         </Link>
                     ))}
                 </div>
